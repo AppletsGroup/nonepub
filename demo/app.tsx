368bcb39fda2f9b8671129b577f19183e0cc797d
zhuscat <zhudx6512@163.com>
2021-10-14 00:06:54 +0800

    feat: demo增加了序列化的示例

--- demo/app.tsx
+++ demo/app.tsx
@@ -15,9 +15,24 @@ export default function App() {
     console.log(editor.getContentHtml())
   }, [editor])
 
+  const handleOutputJSON = useCallback(() => {
+    console.log(editor.getContentJSON())
+  }, [editor])
+
   return (
     <div className="app">
-      <button onClick={handleOutputHtml}>输出HTML内容</button>
+      <button
+        onClick={handleOutputHtml}
+        className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700 mr-1"
+      >
+        输出HTML内容
+      </button>
+      <button
+        onClick={handleOutputJSON}
+        className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"
+      >
+        输出JSON内容
+      </button>
       <EditorProvider editor={editor}>
         <EditorContent />
       </EditorProvider>
